e46692ddb189b732d81d2f62cf6c9b7029c08de8
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2023-09-20 22:51:42 +0500

    Add project files.

---
+++ Tyuiu.AleevRI.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AleevRI.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.AleevRI.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.AleevRI.Sprint1.Task0.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AleevRI.Sprint1.Task0.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AleevRI.Sprint1.Task0.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ddd61f1-7a8d-4d78-844a-7cb0f292df1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
