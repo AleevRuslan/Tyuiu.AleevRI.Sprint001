e941944449e6097eea2b7f971486953f9fa018d8
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2023-09-22 00:35:10 +0500

    Task2

---
+++ Tyuiu.AleevRI.Sprint1.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AleevRI.Sprint1.Task2.V2.Lib;
+
+namespace Tyuiu.AleevRI.Sprint1.Task2.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 90;
+            var res = ds.ConvertAngleToRad(x);
+            Assert.AreEqual(1.57, res);
+
+        }
+
+    }
+}
---
+++ Tyuiu.AleevRI.Sprint1.Task2.V2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AleevRI.Sprint1.Task2.V2.Lib;
+
+namespace Tyuiu.AleevRI.Sprint1.Task2.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Алеев Р. И. | АСОиУБ-23-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #1                                                            *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                   *");
+            Console.WriteLine("* Задание #1.2                                                         *");
+            Console.WriteLine("* Вариант #2                                                           *");
+            Console.WriteLine("* Выполнил: Алеев Руслан Ильнурович | АСОиУБ-23-1                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные      *");
+            Console.WriteLine("* данные, выполняет указанные расчёты и печатает результат на экране.  *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Угол в радианах = " + ds.ConvertAngleToRad(x));
+
+            Console.ReadLine();
+        }
+    }
+}
